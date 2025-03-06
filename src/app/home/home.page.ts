581b036ffcff73906a07f95e617ed449ac162218
bwernee <bernieleen.06@gmail.com>
2025-03-07 02:10:54 +0800

    llevado_geolocator

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,32 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  location: any;
 
   constructor() {}
+ async getCurrentLocation(){
+  try {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current Position: ' +coordinates);
+    console.log(coordinates);
 
+    const {latitude , longitude} = coordinates.coords;
+    alert('Latitude: $(latitude), Longitude: $(longitude)');
+  } catch (error) {
+    alert('Error getting location:'+error);
+  }
+ }
+
+
+
+async watchLocation() {
+  const watchId = Geolocation.watchPosition({}, (position, err) => {
+  if (err){
+    console.log("Error watching position: ",err);
+    return;
+  }
+  console.log('Watch Position: ',position);
+  this.location =position?.coords;
+  })
+}
 }
